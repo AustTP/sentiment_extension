daf19131f44894d3e1a824c71e827090514cdf82
Austin Poole <austtp25@gmail.com>
2020-08-18 09:18:58 -0400

    Update content.js

--- Chrome/content.js
+++ Chrome/content.js
@@ -36,6 +36,29 @@ function save() {
 			var pull = read("today");
 			pull = JSON.parse(pull);
 			
+			if (pull['date'] != date.toLocaleDateString()) {
+				if (localStorage.getItem('full') === null) {
+					var bookmarks = [];
+				} else {
+					var bookmarks = JSON.parse(localStorage.getItem('full'));
+				}
+
+				bookmarks.push(pull);
+				update('full', JSON.stringify(bookmarks));
+				
+				var fetch = JSON.parse(localStorage.getItem('full'));
+				console.log(fetch);
+				
+				// var individuals = fetch[9];
+				// console.log(individuals["score"]);
+				
+				var reboot = {'date': date.toLocaleDateString(), 'score': 0, 'wordCount': 0};
+				update('today', JSON.stringify(reboot));
+				
+				pull = read("today");
+				pull = JSON.parse(pull);
+			}
+			
 			chrome.runtime.sendMessage({"message": "getScore", "text": data[time].replace("undefined", ""), "charCount": charCount, "wordCount": pull['wordCount'], "score": pull['score']}, function(response) {
 				var score = JSON.stringify(response["Summary"]);
 				var parse = JSON.parse(score);
@@ -43,7 +66,7 @@ function save() {
 				var object = {'date': date.toLocaleDateString(), 'score': parse["score"], 'wordCount': parse["wordCount"]};
 				update('today', JSON.stringify(object));
 				
-				chrome.runtime.sendMessage({"message":"returnScore", "wordCount": parse["wordCount"], "score": parse["score"]});
+				chrome.runtime.sendMessage({"message": "returnScore", "wordCount": parse["wordCount"], "score": parse["score"]});
 			});
 		}
 		
@@ -68,22 +91,7 @@ setInterval(function() {
 // }, 600000);
 
 setInterval(function() {
-	if (localStorage.getItem('austin') === null) {
-		var bookmarks = [];
-	} else {
-		var bookmarks = JSON.parse(localStorage.getItem('austin'));
-	}
-
-	bookmarks.push(read('today'));
-	
-	update('austin', JSON.stringify(bookmarks));
-	
-	var notnow = JSON.parse(localStorage.getItem('austin'));
-	console.log(notnow);
-
-	var test = JSON.parse(notnow[0]);
-	console.log(test["score"]);
-}, 20000);
+}, 600000);
 
 setInterval(function() {
 	
@@ -95,12 +103,12 @@ function update(key, value) {
 
 function read(key) {
 	if (localStorage.getItem(key) === null) {
-		return [];
+		return 0;
 	} else {
 		return localStorage.getItem(key);
 	}
 }
 
 function remove(key) {
-	localStorage.removeItem(key);
+	return localStorage.removeItem(key);
 }

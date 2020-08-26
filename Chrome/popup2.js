0f3b7ee7b4acdc84e30a7da356238d3feff893b4
Austin Poole <austtp25@gmail.com>
2020-08-26 08:06:44 -0400

    Update popup2.js

--- Chrome/popup2.js
+++ Chrome/popup2.js
@@ -1,6 +1,7 @@
 var toSave = {};
 var calendar = {};
 var today = [];
+var temp = [];
 var full = [];
 var d = new Date();
 var startPatterns = [d.getMonth() + 1, d.getMonth()];
@@ -8,33 +9,33 @@ var startPatterns = [d.getMonth() + 1, d.getMonth()];
 document.addEventListener('DOMContentLoaded', function() {
 	chrome.storage.sync.get(["toSave"], function(logs) {
 		for (var key in logs) {
-			today = today.push(...logs[key]);
+			today.push(...logs[key]);
 		}
 	});
 	
 	chrome.storage.sync.get(["calendar"], function(logs) {
 		for (var key in logs) {
-			full = full.push(...logs[key]);
+			full.push(...logs[key]);
 		}
 	});
-	
-	// var alter = function(element) {
-		// return element.date == d.toLocaleDateString(); //use the argument here.
-	// }
-	
-	// let filtered = today.filter(name => (startPatterns.some(pattern => name.date.startsWith(pattern))));
-	// let filtered = today.filter(alter);
-	// console.log(filtered);
+});
 
+window.addEventListener("load", function(event) {
+	full = full.filter(name => (startPatterns.some(pattern => name.date.startsWith(pattern))));
+	// full.pop();
+	
+	console.log(today);
+	console.log(full);
+	
 	addScoreToUI(0, 0);
 });
 
 chrome.runtime.onMessage.addListener (
-	function (request, sender, sendResponse) {		
+	function (request, sender, sendResponse) {
 		if (request.message === "returnScore") {
 			today.push({"date": request.date, "wordCount": request.wordCount, "score": request.score});
-			
-			const res = Object.values(today.reduce((acc, {wordCount, score, ...r}) => {
+
+			today = Object.values(today.reduce((acc, {wordCount, score, ...r}) => {
 				const key = JSON.stringify(r);
 				acc[key] = (acc[key]  || {...r, wordCount: 0, score: 0});
 
@@ -43,31 +44,25 @@ chrome.runtime.onMessage.addListener (
 				return (acc[key].wordCount += wordCount, acc[key].score = maths.toFixed(2), acc);
 			}, {}));
 
-			let redCars = res.filter(redCars => redCars.date != d.toLocaleDateString());
-			let notRed = res.find(notRed => notRed.date == d.toLocaleDateString());
+			let redCars = today.filter(redCars => redCars.date != d.toLocaleDateString());
+			let notRed = today.find(notRed => notRed.date == d.toLocaleDateString());
 
 			if (redCars.length > 0) {
-				today = [];
-				today.push(notRed);
-				
+				temp.push(notRed);
+
 				full = full.concat(redCars);
 
 				chrome.storage.sync.set({"calendar": full}, function() { console.log("Calendar", full); });
-				
-				// res.shift();
-				chrome.storage.sync.set({"toSave": today}, function() { console.log("Today", today); });
-				
-				addScoreToUI(2, 2);
+				chrome.storage.sync.set({"toSave": temp}, function() { console.log("Today", temp); });
 			} else {
-				chrome.storage.sync.set({"toSave": res}, function() { console.log("Today", res); });
-				addScoreToUI(1, 1);
+				chrome.storage.sync.set({"calendar": full}, function() { console.log("Calendar", full); });
+				chrome.storage.sync.set({"toSave": today}, function() { console.log("Today", today); });
 			}
-		}		
+		}
 	}
 );
 
 function addScoreToUI(score, wordCount) {
-    console.log("Adding to ui");
     var para = document.createElement("p");
 
     var node = document.createTextNode(score + "_" + wordCount);
@@ -76,14 +71,8 @@ function addScoreToUI(score, wordCount) {
 
     var element = document.getElementById("daScore");
 
-    // First remove any existing score before adding new one
     if(element.firstChild) element.removeChild(element.firstChild);
     element.appendChild(para)
-    // element.style.display = "block";
-}
-
-function combinedArray(array1, array2) {
-  return [...array1, ...array2];
 }
 
 function wait(ms){

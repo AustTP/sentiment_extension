788bea774fae30403da125abce512b910671cbdf
Austin Poole <austtp25@gmail.com>
2020-08-31 09:09:23 -0400

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -72,7 +72,7 @@ chrome.runtime.onMessage.addListener (
 		if (request.message === "returnScore") {
 			today.push({"date": request.date, "wordCount": request.wordCount, "score": request.score});
 			
-			res = Object.values(today.reduce((acc, {wordCount, score, ...r}) => {
+			var res = Object.values(today.reduce((acc, {wordCount, score, ...r}) => {
 				const key = JSON.stringify(r);
 				acc[key] = (acc[key]  || {...r, wordCount: 0, score: 0});
 

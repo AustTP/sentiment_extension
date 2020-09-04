d3ffb655eb5c30f0335882b4434697cff9dce78e
Austin Poole <austtp25@gmail.com>
2020-09-04 09:36:46 -0400

    Update background.js

--- Chrome/background.js
+++ Chrome/background.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	chrome.storage.sync.get(["calendar"], function(logs) {
 		for (var key in logs) {
-			if (logs.hasOwnProperty(key) && (logs[key].toString().indexOf(startPatterns[0]) || logs[key].toString().indexOf(startPatterns[1])) && logs[key] != d.toLocaleDateString()) {
+			if (logs.hasOwnProperty(key) && (logs[key].toString().indexOf(startPatterns[0]) || logs[key].toString().indexOf(startPatterns[1]))) {
 				full.push(...logs[key]);
 			}
 		}
